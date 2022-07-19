04c7e26cc17d562134dc0808de8f29891726e032
Dora <107618912+doraITman@users.noreply.github.com>
2022-07-19 17:06:44 +0300

    Add project files.

---
+++ BookExercise/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BookExercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookExercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm([FromBody]GuestResponse guestResponse)
+        {
+            Repoitory.AddResponse(guestResponse);
+            return View("Thanks",guestResponse);
+        }
+        
+    }
+}
---
+++ BookExercise/Models/Repoitory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BookExercise.Models
+{
+    public static class Repoitory
+    {
+       
+         private static List<GuestResponse> responses=new List<GuestResponse>();
+            public static IEnumerable<GuestResponse> Responses
+            {
+                get { 
+                    return responses;
+                }
+            }
+            public static void AddResponse(GuestResponse response)
+            {
+                responses.Add(response);
+            }   
+  
+    }
+}
