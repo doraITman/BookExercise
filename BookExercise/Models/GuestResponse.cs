35f0dc23fea3e7f537753e4879fb7fb600a1f00d
Dora <107618912+doraITman@users.noreply.github.com>
2022-07-21 18:37:27 +0300

    Додав перевірки у форму

--- BookExercise/Models/GuestResponse.cs
+++ BookExercise/Models/GuestResponse.cs
@@ -1,10 +1,15 @@
-﻿namespace BookExercise.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace BookExercise.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="PLease enter your name")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter ypu email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage =  "please enter valid email")]
         public string Email { get; set; }
         public string Phone { get; set; }
+        [Required(ErrorMessage ="Please enter your choose")]
         public bool? WillAttend { get; set; }
 
         public int MyProperty1 { get; set; }
